213024730349adc01c9daf5c4d6764f6f65cd69f
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2024-10-12 00:27:26 -0500

    ajustes

--- src/main/java/com/cibertec/rest/entity/Usuario.java
+++ src/main/java/com/cibertec/rest/entity/Usuario.java
@@ -2,16 +2,15 @@ package com.cibertec.rest.entity;
 
 import java.util.Date;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import lombok.Getter;
 import lombok.Setter;
 
